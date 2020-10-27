6d89f491f6e7cfaddbd11189ee8b02b487a5d215
Willsem <happymainer@gmail.com>
2020-10-27 14:24:15 +0300

    :pencil: fix write to file

--- lab_02/file.py
+++ lab_02/file.py
@@ -1,9 +1,14 @@
 def encode_file(enigma, filename, outfile):
     input_file = open(filename, 'rb')
     output_file = open(outfile, 'wb')
-    byte = input_file.read(10)
+    byte = input_file.read(100)
     while byte:
-        output_file.write(enigma.encode(byte))
-        byte = input_file.read(10)
+        bytes_to_write = b""
+        for symbol in byte:
+            char = chr(symbol)
+            char = enigma.encode(char)
+            bytes_to_write += bytes([ord(char)])
+        output_file.write(bytes_to_write)
+        byte = input_file.read(100)
     input_file.close()
     output_file.close()
