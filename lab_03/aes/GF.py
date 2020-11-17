d561964259ba4c9d409d2b9d4633e2925726c1cd
Willsem <happymainer@gmail.com>
2020-11-17 21:42:00 +0300

    :bug: fix decode function

--- lab_03/aes/GF.py
+++ lab_03/aes/GF.py
@@ -16,12 +16,15 @@ def mul_by_09(num):
 
 
 def mul_by_0b(num):
-    return mul_by_09(num) ^ mul_by_02(num)
+    return mul_by_02(mul_by_02(mul_by_02(num))) \
+        ^ mul_by_02(num) ^ num
 
 
 def mul_by_0d(num):
-    return mul_by_0b(num) ^ mul_by_02(num)
+    return mul_by_02(mul_by_02(mul_by_02(num))) ^ \
+        mul_by_02(mul_by_02(num)) ^ num
 
 
 def mul_by_0e(num):
-    return mul_by_0d(num) ^ num
+    return mul_by_02(mul_by_02(mul_by_02(num))) ^ \
+        mul_by_02(mul_by_02(num)) ^ mul_by_02(num)
