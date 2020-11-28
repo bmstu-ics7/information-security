00599b1ea55ec9986f61c69d5282c4de7e74d985
Willsem <happymainer@gmail.com>
2020-11-28 23:05:35 +0300

    :new: add lab_05

---
+++ lab_05/Options.cs
@@ -0,0 +1,16 @@
+using CommandLine;
+
+namespace DigitalSignature
+{
+    public class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "File for check or create.")]
+        public string File { get; set; }
+
+        [Option('k', "key", Required = true, HelpText = "File with key for check or create.")]
+        public string KeyFile { get; set; }
+
+        [Option('c', "check", Required = false, HelpText = "Set for check file.")]
+        public bool Check { get; set; }
+    }
+}
---
+++ lab_05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CommandLine;
+
+namespace DigitalSignature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunProgram);
+        }
+
+        static void RunProgram(Options opts)
+        {
+            try
+            {
+                if (opts.Check)
+                {
+                    Signature.Check(opts.File, opts.KeyFile);
+                }
+                else
+                {
+                    Signature.Create(opts.File, opts.KeyFile);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
